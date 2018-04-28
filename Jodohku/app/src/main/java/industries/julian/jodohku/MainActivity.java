67cd39390c422686c6c48a605fe7737fc67da680
KATONGILANG\KatonGilang <katon.gilang@ti.ukdw.ac.id>
2018-04-28 21:38:55 +0700

    updated Saturday Night, 28 April at 21:38 WIB

--- Jodohku/app/src/main/java/industries/julian/jodohku/MainActivity.java
+++ Jodohku/app/src/main/java/industries/julian/jodohku/MainActivity.java
@@ -1,26 +1,19 @@
 package industries.julian.jodohku;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.CardView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private CardView menuMap,menuProfil,menuCari;
+
+    private CardView menuMap,menuProfil,menuCari,menuChat;
     private Button keluar;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
@@ -35,12 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         menuMap = (CardView) findViewById(R.id.cardMap);
         menuProfil = (CardView) findViewById(R.id.cardProfil);
         menuCari = (CardView) findViewById(R.id.cardCari);
+        menuChat = (CardView) findViewById(R.id.cardChat);
 
 
         keluar.setOnClickListener(this);
         menuMap.setOnClickListener(this);
         menuProfil.setOnClickListener(this);
         menuCari.setOnClickListener(this);
+        menuChat.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -54,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnKeluar:
                 i = new Intent(this, LoginActivity.class);
                 FirebaseAuth.getInstance().signOut();
+                toastMessage("Proses keluar ..");
                 startActivity(i);
                 break;
             case R.id.cardProfil:
@@ -61,11 +57,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
             case R.id.cardCari:
-                i = new Intent(this, CariTemanActivity.class);
+                i = new Intent(this, SaranTemanActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardChat:
+                i = new Intent(this, ChatActivity.class);
                 startActivity(i);
                 break;
             default:
                 break;
         }
+    }
+   /* public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    public void onStop(){
+        super.onStop();
+        if (firebaseAuthStateListener != null){
+            mAuth.removeAuthStateListener(firebaseAuthStateListener);
+        }
+    }*/
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
